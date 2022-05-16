7c1bf797f0763b536379dfba569f78d58a9841cc
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-16 15:33:00 +0530

    commit

--- src/main/java/com/example/finalgradlejetty/App.java
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -1,20 +1,27 @@
 package com.example.finalgradlejetty;
 
+import jakarta.servlet.Servlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import javax.servlet.http.HttpServletMapping;
+import java.net.URL;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
         Server server = new Server(8080);
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/hello-servlet");
+        WebAppContext webAppContext = new WebAppContext();
+        //ServletHolder servletHolder = new ServletHolder((Class<? extends Servlet>) HelloServlet.class);
+        webAppContext.setServer(server);
+        webAppContext.setContextPath("/hello-servlet");
+        webAppContext.setWar("src/main/webapp");
+        webAppContext.addServlet((Class)HelloServlet.class, "/hello-servlet");
 
-        server.setHandler(context);
+        server.setHandler(webAppContext);
         server.start();
         server.join();
     }
