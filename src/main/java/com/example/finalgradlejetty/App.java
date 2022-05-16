5b03f75f7f96a6b68372a9953cd630cefc8a3c36
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-17 02:34:30 +0530

    commit

--- src/main/java/com/example/finalgradlejetty/App.java
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -12,11 +12,11 @@ public class App {
         Server server = new Server(8080);
         WebAppContext webAppContext = new WebAppContext();
 
-        URL webDir = App.class.getClassLoader().getResource("com.example.finalgradlejetty.HelloServlet");
+        //URL webDir = App.class.getClassLoader().getResource("com.example.finalgradlejetty.HelloServlet");
         //context.setResourceBase(webDir.toURI().toString());
         webAppContext.setContextPath("/");
         webAppContext.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-        webAppContext.setResourceBase(webDir.toURI().toString());
+        webAppContext.setResourceBase("com/example/finalgradlejetty/HelloServlet.java");
         webAppContext.setParentLoaderPriority(true);
 
         server.setHandler(webAppContext);
