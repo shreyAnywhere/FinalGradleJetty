803ae73444735d248444d8db1ee0565bf384139b
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-16 13:25:17 +0530

    commit

--- src/main/java/com/example/finalgradlejetty/App.java
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -1,19 +1,21 @@
 package com.example.finalgradlejetty;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
+
+import javax.servlet.http.HttpServletMapping;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
         Server server = new Server(8080);
-//        WebAppContext context = new WebAppContext();
-//        context.setContextPath("/");
-//
-//        server.setHandler(context);
+        WebAppContext  context=new WebAppContext();
+        context.setContextPath("/hello-servlet");
+
+        server.setHandler(context);
         server.start();
-        System.out.println("Server Started ! ");
         server.join();
     }
 }
