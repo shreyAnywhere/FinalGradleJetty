f247d9e995717bd916c1bf6a5f9b9d1d191dd0b6
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-16 12:43:29 +0530

    first commit

---
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -0,0 +1,19 @@
+package com.example.finalgradlejetty;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8080);
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+
+        server.setHandler(context);
+        server.start();
+        System.out.println("Server Started ! ");
+        server.join();
+    }
+}
