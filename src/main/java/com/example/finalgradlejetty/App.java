1450ce1e410dd3b17eec425742c73d2b4062ed41
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-16 18:09:42 +0530

    commit

--- src/main/java/com/example/finalgradlejetty/App.java
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -11,11 +11,11 @@ public class App {
 
         Server server = new Server(8080);
         WebAppContext webAppContext = new WebAppContext();
-        //URL webDir = App.class.getClassLoader().getResource("com/example/finalgradlejetty/HelloServlet.java");
-        //webAppContext.setResourceBase(webDir.toURI().toString());
+        URL webDir = App.class.getClassLoader().getResource("C:\\Users\\HP\\IdeaProjects\\FinalGradleJetty\\src\\main\\java\\com\\example\\finalgradlejetty\\HelloServlet.java");
+        webAppContext.setResourceBase(webDir.toURI().toString());
 
         webAppContext.setServer(server);
-        webAppContext.setContextPath("com/example/finalgradlejetty/HelloServlet.java");
+        webAppContext.setContextPath("/");
         webAppContext.setWar("src/main/webapp");
         webAppContext.addServlet((Class)HelloServlet.class, "/hello-servlet");
 
