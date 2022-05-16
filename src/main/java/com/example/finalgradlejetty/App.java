f4a61f6fad4d83549882dd0b22af6e097110570a
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-16 13:08:26 +0530

    commit

--- src/main/java/com/example/finalgradlejetty/App.java
+++ src/main/java/com/example/finalgradlejetty/App.java
@@ -8,10 +8,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Server server = new Server(8080);
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/");
-
-        server.setHandler(context);
+//        WebAppContext context = new WebAppContext();
+//        context.setContextPath("/");
+//
+//        server.setHandler(context);
         server.start();
         System.out.println("Server Started ! ");
         server.join();
